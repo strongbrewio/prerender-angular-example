8a50a22b91206dffce782a1a45038f3df10a67fb
billietbrecht <billietbrecht@gmail.com>
2018-03-04 10:47:52 +0100

    updated prerender.ts

--- prerender-angular-example/prerender.ts
+++ prerender-angular-example/prerender.ts
@@ -5,28 +5,33 @@ import { enableProdMode } from '@angular/core';
 import { renderModuleFactory } from '@angular/platform-server';
 import { AppPrerenderModuleNgFactory } from './dist-prerender/main.bundle';
 
-const dist = './dist';
+const distFolder = './dist';
 const index = fs
-    .readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf8')
+    .readFileSync(path.resolve(__dirname, `${distFolder}/index.html`), 'utf8')
     .toString();
 
+// you could automate this based on the app.routes.ts file but
+// to keep it simple let's just create an array with the routes we want
+// to prerender
 const paths = [
     '/about',
     '/brews',
     '/consultancy'];
 enableProdMode();
 
-paths.forEach(p => renderToHtml(p, dist + p));
+// for every route render the html and save it in the correct folder
+paths.forEach(p => renderToHtml(p, distFolder + p));
 
 // don't forget to overwrite the index.html as well
-renderToHtml('/index.html', dist);
+renderToHtml('/index.html', distFolder);
 
 function renderToHtml(url: string, folderPath: string): void {
+    // Render the module with the correct url just as the server would do
     renderModuleFactory(AppPrerenderModuleNgFactory, {
         url,
         document: index
     }).then(html => {
-        // the root folder always exists
+        // create the route directory
         if (url !== '/index.html') {
             fs.mkdirSync(folderPath);
         }
